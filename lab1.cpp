326ff73aab45f0e2c2945586fa8515c832793121
pedroschiavom <31569709+pedroschiavom@users.noreply.github.com>
2018-04-05 14:54:24 -0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+  
+{
+   	string line;
+	ifstream myfile ("dictionary.txt");
+	if (myfile.is_open()){
+		while (getline (myfile,line)){
+			cout << line << '\n';
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+	
+	
+	
+	cout << "Procurar palavra: ";
+	
+
+	system("PAUSE");
+	return 0;
+	
+
+}
